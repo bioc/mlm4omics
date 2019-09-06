1f0d11e46b3f747f47318793782bf9e8f3d4943d
lshep <lori.shepherd@roswellpark.org>
2019-09-06 09:00:52 -0400

    Deprecation.

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,11 @@
     modules  <-  paste0("stan_fit4", names(stanmodels), "_mod")
     for (m in modules) loadModule(m, what = TRUE)
 }
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.11")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
